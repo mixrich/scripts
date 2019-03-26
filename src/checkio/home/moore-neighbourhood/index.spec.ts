654cbb8830ea6f45510e93db5ee9f397682c2013
Mickhail Rychagov <mixrich1@gmail.com>
2019-03-26 18:05:58 +0700

    solving neighbourhood task from checkio.org

--- src/checkio/home/moore-neighbourhood/index.spec.ts
+++ src/checkio/home/moore-neighbourhood/index.spec.ts
@@ -1,14 +1,49 @@
 import { assert } from 'chai';
-import {mooreNeighbourhood} from './index';
+import { mooreNeighbourhood } from './index';
 
-const first: number[][] = [
+const matrix5x5: number[][] = [
+  [1, 0, 0, 1, 0],
+  [0, 1, 0, 0, 0],
+  [0, 0, 1, 0, 1],
+  [1, 0, 0, 0, 0],
+  [0, 0, 1, 0, 0]
+];
+
+const dense: number[][] = [
   [1, 1, 1],
   [1, 1, 1],
   [1, 1, 1],
 ];
 
+const single: number[][] = [
+  [0, 0, 0],
+  [0, 1, 0],
+  [0, 0, 0],
+];
+
+const matrix5x6 = [
+  [1,0,1,0,1],
+  [0,1,0,1,0],
+  [1,0,1,0,1],
+  [0,1,0,1,0],
+  [1,0,1,0,1],
+  [0,1,0,1,0]
+];
+
 describe('Moore-neighbourhood', () => {
-    it('should return 3 for 3x3 matrix with all items === 1 for element with coords {x: 0, y: 0}', () => {
-        assert.equal(mooreNeighbourhood(first, 0, 0), 3);
-    });
+  it('should return 3 for matrix5x5 with coords {x: 1, y: 2}', () => {
+    assert.equal(mooreNeighbourhood(matrix5x5, 1, 2), 3);
+  });
+  it('should return 1 for matrix5x5 with coords {x: 0, y: 0}', () => {
+    assert.equal(mooreNeighbourhood(matrix5x5, 0, 0), 1);
+  });
+  it('should return 3 for Dense corner matrix with coords {x: 0, y: 2}', () => {
+    assert.equal(mooreNeighbourhood(dense, 0, 2), 3);
+  });
+  it('should return 0 for single matrix with coords {x: 1, y: 1}', () => {
+    assert.equal(mooreNeighbourhood(single, 1, 1), 0);
+  });
+  it('should return 2 for matrix5x6 with coords {x: 4, y: 5}', () => {
+    assert.equal(mooreNeighbourhood(matrix5x6, 5, 4), 2);
+  });
 });
--- src/checkio/home/moore-neighbourhood/index.ts
+++ src/checkio/home/moore-neighbourhood/index.ts
@@ -1,5 +1,31 @@
-export function mooreNeighbourhood(matrix: number[][], x: number, y: number) {
-    return 3;
+
+/**
+ * https://js.checkio.org/mission/count-neighbours
+ * @param matrix
+ * @param x
+ * @param y
+ */
+export function mooreNeighbourhood(matrix: number[][], y: number, x: number) {
+    const neight = [
+        [-1, -1], [0, -1], [1, -1],
+        [-1, 0], [1, 0],
+        [-1, 1], [0, 1], [1, 1]
+    ];
+
+    let sum = 0;
+
+    neight.forEach((diff) => {
+        const iCol = x + diff[0];
+        const iRow = y + diff[1];
+
+        const value = matrix[iRow] && matrix[iRow][iCol];
+
+        if (value) {
+            sum++;
+        }
+    })
+
+    return sum;
 }
 
 export default mooreNeighbourhood;
