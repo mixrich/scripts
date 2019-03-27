2822f1387582a3dbd42cc7f7c740228eb041ecb8
Mickhail Rychagov <mixrich1@gmail.com>
2019-03-27 17:53:51 +0700

    Creating tests for WeakPoint task

---
+++ src/checkio/home/weak-point/index.spec.ts
@@ -0,0 +1,36 @@
+import { assert } from 'chai';
+import { weakPoint } from './index';
+
+const example = [
+    [7, 2, 7, 2, 8],
+    [2, 9, 4, 1, 7],
+    [3, 8, 6, 2, 4],
+    [2, 5, 2, 9, 1],
+    [6, 6, 5, 4, 5]
+];
+
+const twoPointsExample = [
+    [7, 2, 4, 2, 8],
+    [2, 8, 1, 1, 7],
+    [3, 8, 6, 2, 4],
+    [2, 5, 2, 9, 1],
+    [6, 6, 5, 4, 5]
+];
+
+const topLeft = [
+    [1, 1, 1],
+    [1, 1, 1],
+    [1, 1, 1]
+];
+
+describe('Weak Point', () => {
+    it('should return [3, 3] for Example', () => {
+        assert.deepEqual(weakPoint(example), [3, 3]);
+    });
+    it('should return [1, 2] for TwoPointsExample', () => {
+        assert.deepEqual(weakPoint(example), [1, 2]);
+    });
+    it('should return [0, 0] for TopLeft', () => {
+        assert.deepEqual(weakPoint(example), [0, 0]);
+    });
+});
---
+++ src/checkio/home/weak-point/index.ts
@@ -0,0 +1,6 @@
+export function weakPoint(map: number[][]): [number, number] {
+
+    return [0, 0];
+}
+
+export default weakPoint;
